fa7baa6cdf0376b09f85d2fbf5e58658fe0c3596
VladimirDanil <vladimir.danil25@mail.ru>
2021-05-31 12:22:20 -0700

    some update

--- frontend/src/components/renderDatePicker.js
+++ frontend/src/components/renderDatePicker.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import DatePicker from 'react-datepicker';
-   
+
 const renderDatePicker = ({input, placeholder, defaultValue, meta: {touched, error} }) => (
   <div>
     <DatePicker {...input} dateForm="MM/DD/YYYY" selected={input.value ? input.value : null} />
