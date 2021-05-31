967bc061e6160f32b9fa0673fc15068549ccf066
VladimirDanil <vladimir.danil25@mail.ru>
2021-05-31 12:20:24 -0700

    fix

--- frontend/src/containers/financial/savings_calculator.js
+++ frontend/src/containers/financial/savings_calculator.js
@@ -11,7 +11,7 @@ import {
   ResponsiveContainer
 } from "recharts";
 import {makeStyles} from "@material-ui/core/styles"
-
+       
 import Nav from '../layout/nav'
 import {isNumeric} from '../../service/textService'
 
